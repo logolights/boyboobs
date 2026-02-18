753e78b2b4b720dc8429f2a2f47d021c8334784b
Benji <benjaminbreaking099@gmail.com>
2026-02-18 16:07:57 -0600

    Delete boyboobs-main directory

--- boyboobs-main/scripts/copyrightYear.js
+++
@@ -1,8 +0,0 @@
-function copyrightYear() {
-
-  document.getElementById("yearText").innerHTML = new Date().getFullYear().toString(); 
-  
-}
-copyrightYear();
-
-//using .toString because getFullYear doesn't return a string
